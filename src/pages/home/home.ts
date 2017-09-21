783f65be1c32479ef14be6844d9c0a0319c8c2c9
lapadol <lapadol@msn.com>
2017-09-21 15:30:08 +0700

    try conect with signa

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { NavController } from 'ionic-angular';
+import * as signalR from '@aspnet/signalr-client';
 
 @Component({
   selector: 'page-home',
@@ -10,5 +11,66 @@ export class HomePage {
   constructor(public navCtrl: NavController) {
 
   }
+
+
+    public counter: number = 0;
+    onTap() {
+
+        this.counter++;
+        alert("Tapped " + this.counter + " times!");
+        this.connection.invoke("Send", "test" + this.counter);
+    }
+
+    StartSignalR() {
+        console.log("start signalr");
+        this.doSigNalR();
+    }
+
+    connection: signalR.HubConnection;
+    transportType: signalR.TransportType;
+
+
+    users: Array<any> = new Array<any>();
+    messages: Array<any> = new Array<any>();
+
+    doSigNalR() {
+        this.transportType = signalR.TransportType.WebSockets;
+        let logger = new signalR.ConsoleLogger(signalR.LogLevel.Information);
+
+        let connectOption = new signalR.HttpClient();
+        console.log(connectOption.options);
+        var Domain = `http://${document.location.host}/chat`;
+        Domain = 'http://localhost:63135/NotificationHub';
+        let http = new signalR.HttpConnection(Domain, { transport: this.transportType, logging: logger });
+
+        this.connection = new signalR.HubConnection(http);
+
+        this.connection.onClosed = e => {
+            if (e) {
+                this.appendLine('Connection closed with error: ' + e, 'red');
+            }
+            else {
+                this.appendLine('Disconnected', 'green');
+            }
+        };
+
+        this.connection.on('Send', (userName, message) => {
+            this.messages.push(userName + ':' + message);
+        });
+
+        this.connection.start()
+            .then(data => {
+                this.connection.invoke("RegisterUserMap",1,1);
+            })
+            .catch(err => {
+                this.appendLine(err, 'red');
+                this.connection.stop();
+            });
+    }
+    appendLine(line: any, color?: any) {
+
+        this.messages.push(line);
+        console.log(this.messages);
+    };
 
 }
